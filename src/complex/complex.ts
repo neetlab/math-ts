f01b15fb02108cb680604587a11a17b0ff009bcb
Ryō Igarashi <n33t5hin@gmail.com>
2020-06-28 17:29:17 +0900

    Add complex number

---
+++ src/complex/complex.ts
@@ -0,0 +1,24 @@
+import { Eq, Sum, Prod } from "../interfaces";
+
+export class Complex implements Eq<Complex>, Sum<Complex>, Prod<Complex> {
+  constructor(readonly real: number, readonly imagine: number) {}
+
+  equals(that: Complex) {
+    return this.real === that.real && this.imagine === that.imagine;
+  }
+
+  add(that: Complex) {
+    return new Complex(this.real + that.real, this.imagine + that.imagine);
+  }
+
+  multiply(that: Complex) {
+    if (this.imagine === that.imagine) {
+      return new Complex(this.real * that.real * -1, 0);
+    }
+
+    return new Complex(
+      this.real * that.real - this.imagine * that.imagine,
+      this.real * that.imagine + this.imagine * that.real
+    );
+  }
+}
