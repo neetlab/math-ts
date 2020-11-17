42a72c7c5cd861e13a269ecf311ed87fb9c988bb
Ryo Igarashi <n33t5hin@gmail.com>
2020-11-17 18:17:36 +0900

    Add convergence test to probability

---
+++ src/_utils/convergence.ts
@@ -0,0 +1,13 @@
+import { SampleSpace, Event } from "../probability";
+
+export const testConvergence = (sampleSpace: SampleSpace, event: Event, repeat = 1000) => {
+  let trueCount = 0;
+
+  for (let i = 0; i < repeat; i++) {
+    if (sampleSpace.experiment(event).result) {
+      trueCount++;
+    };
+  }
+
+  console.log(`Expected ${event.probability} / Got ${trueCount / repeat}`);
+}
