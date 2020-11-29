eb6ae63168aff0e758179ff7d8d91ad20443fc0d
Ryo Igarashi <n33t5hin@gmail.com>
2020-11-29 11:59:42 +0900

    Add fraction / irrational function

--- src/function/function1.ts
+++ src/function/function1.ts
@@ -1,4 +1,4 @@
-import { Expression, Variable } from "../expression";
+import { Expression } from "../expression";
 
 export class Interval {
   constructor(
@@ -37,5 +37,43 @@ export class Function1 {
   inverse() {
     // TODO: 指数とか係数とか定数項を全部払って定義域と値域を逆にする
     return new Function1(this.expression, this.range, this.domain);
+  }
+}
+
+export class IrrationalFunction1 {
+  constructor(
+    // inside root
+    private readonly expression: Expression,
+    readonly root: number,
+    readonly operator: '+' | '-',
+  ) {}
+
+  call(value: number) {
+    // within domain, the √ will never be negative
+    const leaf = this.expression
+      .substitute(new Map([['x', value]]))
+      .toNumber();
+    
+    return (this.operator === '+' ? 1 : -1) * Math.pow(leaf, 1 / this.root);
+  }
+}
+
+export class FractionFunction1 {
+  constructor(
+    readonly numerator: Expression,
+    readonly denominator: Expression,
+    readonly rest: Expression,
+  ) {}
+
+  call(value: number) {
+    const x = new Map([['x', value]]);
+    const n = this.numerator.substitute(x).toNumber();
+    const d = this.numerator.substitute(x).toNumber();
+    const r = this.rest.substitute(x).toNumber();
+
+    // domain
+    if (d === 0) throw RangeError('x is out of domain');
+
+    return (n / d) + r;
   }
 }
