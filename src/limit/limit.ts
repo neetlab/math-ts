c043526d7baf435386532da28f4cc2508eae0b41
Ryo Igarashi <n33t5hin@gmail.com>
2020-11-30 16:44:44 +0900

    Add sigma data type

--- src/limit/limit.ts
+++ src/limit/limit.ts
@@ -1,5 +1,6 @@
 import { Function1 } from "src/function/function1";
 import { range } from "../integer";
+import { Sigma } from '../sigma';
 import { ArithmeticSequence, GeometricSequence, Sequence } from "../sequence";
 
 export enum InfinityType {
@@ -15,15 +16,18 @@ const pairwise = <T>(array: T[]) => {
     .filter((value): value is [T, T] => value != null);
 }
 
-export function lim(arrow: number, arithmetic: ArithmeticSequence): number;
 export function lim(arrow: number, geometric: GeometricSequence): number;
+export function lim(arrow: number, sigma: Sigma): number;
 export function lim(arrow: number, func: Function1): number;
 export function lim(arrow: number, value: unknown): number {
   const seq  = value;
+  const sigma = value;
   const func = value;
 
   // 無限等比数列
-  if (seq instanceof GeometricSequence && seq.length === Infinity) {
+  if ( seq instanceof GeometricSequence
+    && seq.length === Infinity
+  ) {
     if (seq.ratio > 1)           return Infinity;
     if (seq.ratio === 1)         return 1;
     if (Math.abs(seq.ratio) < 1) return 0;
@@ -31,16 +35,21 @@ export function lim(arrow: number, value: unknown): number {
   }
 
   // 無限級数
-  if (seq instanceof ArithmeticSequence && true /* sigma(arith) */) {
-    // これは無理
-    // 部分分数分解みたいな方法で証明している
-    return NaN;
+  if ( sigma instanceof Sigma
+    && sigma.seq instanceof ArithmeticSequence
+    && sigma.to === Infinity
+  ) {
+    // これは無理, 部分分数分解みたいな方法で証明している
+    return sigma.evaluate();
   }
 
   // 無限等比級数
-  if (seq instanceof GeometricSequence && true /* Sigma(GeometricSequence) && seq.length == ∞ */) {
-    if (Math.abs(seq.ratio) < 1)  return seq.first / (1 - seq.ratio);
-    if (Math.abs(seq.ratio) >= 1) return NaN;
+  if ( sigma instanceof Sigma
+    && sigma.seq instanceof GeometricSequence
+    && sigma.to === Infinity
+  ) {
+    if (Math.abs(sigma.seq.ratio) < 1)  return sigma.seq.first / (1 - sigma.seq.ratio);
+    if (Math.abs(sigma.seq.ratio) >= 1) return NaN;
   }
 
   // lim{x->a}f(x)においてaがfの定義域 <=> lim{x->a}f(x) = f(a)
@@ -56,41 +65,46 @@ export function lim(arrow: number, value: unknown): number {
   return NaN;
 }
 
-export const getInfinityType = (arrow: number, sequence: Sequence) => {
-  const limitSeq = pairwise(getCloser(arrow, sequence));
+export function getInfinityType(sequence: Sequence): InfinityType
+export function getInfinityType(sigma: Sigma): InfinityType
+export function getInfinityType(value: unknown): InfinityType {
+  if (value instanceof Sigma) {
+    // 無限等比級数 --> |r|でわかる
+    if (value.seq instanceof GeometricSequence) {
+      if (Math.abs(value.seq.ratio) <  1) return InfinityType.CONVERGE;
+      if (Math.abs(value.seq.ratio) >= 1) return InfinityType.DIVERGE_POSITIVE; // unknown sign;
+    }
 
-  // 正で限りなく大きくなる
-  if (limitSeq.every(([self, next]) => self > 0 && self <= next)) {
-    return InfinityType.DIVERGE_POSITIVE;
+    // 無限級数 --> 一般項の極限が0 ? 収束 : 発散
+    // TODO: lim()が式を取れない
+    if (value.seq instanceof ArithmeticSequence) {
+      return value.seq.getNth(Infinity) === 0
+        ? InfinityType.CONVERGE
+        : InfinityType.DIVERGE_NEGATIVE;
+    }
   }
 
-  // 負で限りなく大きくなる
-  if (limitSeq.every(([self, next]) => self < 0 && Math.abs(self) <= Math.abs(next))) {
-    return InfinityType.DIVERGE_NEGATIVE;
-  }
+  if (value instanceof Sequence) {
+    const limitSeq = pairwise(getCloser(Infinity, value));
 
-  // 一定の値に近づく
-  // next - selfが負のときに > だとハンドルできないから絶対値を付ける...?
-  if (limitSeq.every(([self, next]) => Math.abs(self) > Math.abs(next))) {
-    return InfinityType.CONVERGE;
+    // 正で限りなく大きくなる
+    if (limitSeq.every(([self, next]) => self > 0 && self <= next)) {
+      return InfinityType.DIVERGE_POSITIVE;
+    }
+
+    // 負で限りなく大きくなる
+    if (limitSeq.every(([self, next]) => self < 0 && Math.abs(self) <= Math.abs(next))) {
+      return InfinityType.DIVERGE_NEGATIVE;
+    }
+
+    // 一定の値に近づく
+    // next - selfが負のときに > だとハンドルできないから絶対値を付ける...?
+    if (limitSeq.every(([self, next]) => Math.abs(self) > Math.abs(next))) {
+      return InfinityType.CONVERGE;
+    }
   }
 
   // 振動
-  return InfinityType.OSCILLATE;
-}
-
-export const getInfinityTypeOfSigma = (sequence: Sequence) => {
-  // 無限等比級数 --> |r|でわかる
-  if (sequence instanceof GeometricSequence) {
-    if (Math.abs(sequence.ratio) <  1) return InfinityType.CONVERGE;
-    if (Math.abs(sequence.ratio) >= 1) return InfinityType.DIVERGE_POSITIVE; // unknown sign;
-  }
-
-  // 無限級数 --> 一般項の極限が0 ? 収束 : 発散
-  if (sequence instanceof ArithmeticSequence) {
-    return lim(Infinity, sequence) === 0 ? InfinityType.CONVERGE : InfinityType.DIVERGE_POSITIVE; // unknown sign;
-  }
-
   return InfinityType.OSCILLATE;
 }
 
--- src/sigma/sigma.ts
+++ src/sigma/sigma.ts
@@ -1,3 +1,5 @@
+import { Sequence } from "../sequence";
+
 export const sigma = (k: number, n: number, expr: (n: number) => number) => {
   let i = k;
   let sum = 0;
@@ -8,4 +10,29 @@ export const sigma = (k: number, n: number, expr: (n: number) => number) => {
   }
 
   return sum;
+}
+
+
+export class Sigma {
+  constructor(
+    readonly from: number,
+    readonly to: number,
+    readonly seq: Sequence,
+  ) {}
+
+  evaluate() {
+    let i = this.from;
+    let sum = 0;
+
+    while (i <= this.to) {
+      sum += this.seq.getNth(i);
+      i++;
+    }
+
+    return sum;
+  }
+
+  toString() {
+    return `\\sum_k={${this.from}}^{${this.to}}${this.seq}`;
+  }
 }
