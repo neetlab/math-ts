309f23abbb9d01bdf0b045a1b475f01bd0fded86
Ryo Igarashi <n33t5hin@gmail.com>
2020-12-03 18:42:27 +0900

    Add product of derivative func

--- src/calculus/calculus.spec.ts
+++ src/calculus/calculus.spec.ts
@@ -2,6 +2,7 @@ import { $, where } from '../_utils';
 import { x } from '../expression';
 import { differentiate } from './differential'
 import { integrate, CONSTANT_OF_INTEGRATION as C } from './integral';
+import { Function } from '../function';
 
 describe('calculus', () => {
   test("∫f'(x) = f(x)", () => {
@@ -10,4 +11,16 @@ describe('calculus', () => {
     const F = integrate(fp).d(x);
     expect(f).toEq(F.partial(where(C, 5).toMap()));
   })
+
+  test("{kf(x)}'=kf'(x)", () => {
+    const k  = 4;
+    const f  = $(3, x, 2).plus(2, x).plus(5).toFunction();
+    const kf = new Function(f.expression.multiply(k));
+
+    const v = where(x, 1).toMap();
+    const dfDx = differentiate(f).d(x);
+    const dkfDx = differentiate(kf).d(x);
+
+    expect(dfDx.call(v).toNumber() * k).toBe(dkfDx.call(v).toNumber());
+  });
 });
--- src/calculus/differential.ts
+++ src/calculus/differential.ts
@@ -5,7 +5,11 @@ export const differentiate = (f: Function) => ({
   d: (x: string | symbol) => {
     const expr = new Expression(f.expression.terms.map((t) => {
       if (t instanceof Constant) return new Constant(0);
-      if (t.name === x) return new Variable(t.name, t.exponent, Math.max(0, t.exponent - 1)).product(t.factor);
+      const exponent = Math.max(0, t.exponent - 1);
+      if (t.name === x && exponent <= 0) return new Constant(t.factor);
+      if (t.name === x)  {
+        return new Variable(t.name, t.exponent, exponent).product(t.factor);
+      }
       return t;
     }));
 
